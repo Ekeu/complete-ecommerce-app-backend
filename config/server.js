297978f18b4f2898639755c3c643bb86e1fc4869
Ekeu Ulrich <ulrich.ekeu.monkam@efrei.net>
2021-09-07 14:41:19 +0200

    Initial Strapi server setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd92bcf63f564b13cc7bf1f5fa6a0e915'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4e885cd6-45dc-4bd9-ac82-fc82499bf888'
+};
