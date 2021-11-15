7d747380db5131f64e2a91709c58370e9e5bee57
monks___ <ulrich.ekeu.pro@gmail.com>
2021-11-15 14:05:16 +0100

    Updated strapi by adding hook for algolia

--- api/product/models/product.js
+++ api/product/models/product.js
@@ -1,8 +1,29 @@
-'use strict';
+"use strict";
+
+const moment = require("moment");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const index = "product";
+
+module.exports = {
+  lifecycles: {
+    afterCreate(result, data) {
+      result.createdAt_timestamp = moment(result.createdAt).unix();
+      strapi.services.algolia.saveObject(result, index);
+    },
+    afterUpdate(result, params, data) {
+      if (result.published_at) {
+        strapi.services.algolia.saveObject(result, index);
+      } else {
+        strapi.services.algolia.deleteObject(result.id, index);
+      }
+    },
+    afterDelete(result, params) {
+      strapi.services.algolia.deleteObject(result.id, index);
+    },
+  },
+};
--- api/variant/models/variant.js
+++ api/variant/models/variant.js
@@ -1,8 +1,31 @@
-'use strict';
+"use strict";
+
+const moment = require("moment");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const index = "variant";
+
+module.exports = {
+  lifecycles: {
+    afterCreate(result, data) {
+      result.product.createdAt_timestamp = moment(
+        result.product.createdAt
+      ).unix();
+      strapi.services.algolia.saveObject(result, index);
+    },
+    afterUpdate(result, params, data) {
+      if (result.published_at) {
+        strapi.services.algolia.saveObject(result, index);
+      } else {
+        strapi.services.algolia.deleteObject(result.id, index);
+      }
+    },
+    afterDelete(result, params) {
+      strapi.services.algolia.deleteObject(result.id, index);
+    },
+  },
+};
